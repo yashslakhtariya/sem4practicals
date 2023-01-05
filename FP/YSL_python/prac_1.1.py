bfe337e6e3494041e76087e8d98f0f6b2967e6d3
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2023-01-05 20:08:59 +0530

    updated

--- FP/YSL_python/prac_1.1.py
+++ FP/YSL_python/prac_1.1.py
@@ -1,3 +1,4 @@
+import calendar
 import datetime
 
 bday = datetime.date(1947, 5, 6)
@@ -5,4 +6,7 @@ today = datetime.date.today()
 
 age = int((today - bday).days / 365.2425)
 
-print("The age of Shyam's Grandfather born on {0} as per {1} is {2} years".format(bday, today, age))
+print("\nThe age of Shyam's Grandfather born on {0} as per {1} is {2} years".format(bday, today, age))
+
+print("\nThe calendar of his birth month is : \n" + calendar.month(bday.year, bday.month))
+print("\nThe calendar of his birth year is : \n" + calendar.calendar(bday.year))
