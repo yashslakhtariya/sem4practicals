14ab8717adb2f5089cd72da578a5fadcd4fa6fe2
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2023-01-05 19:12:55 +0530

    updated

--- FP/YSL_python/prac_1.1.py
+++ FP/YSL_python/prac_1.1.py
@@ -1,6 +1,8 @@
 import datetime
 
-bday = datetime.date(1947-6-5)
-
+bday = datetime.date(1947, 5, 6)
 today = datetime.date.today()
-print(today)
+
+age = int((today - bday).days / 365.2425)
+
+print("The age of Shyam's Grandfather born on {0} as per {1} is {2} years".format(bday, today, age))
