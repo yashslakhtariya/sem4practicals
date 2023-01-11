00dc56bc99d98cedca2f2f62c07f55d06f7e1096
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2023-01-11 10:53:44 +0530

    updated

---
+++ FP/YSL_python/demo.py
@@ -0,0 +1,9 @@
+x, y = 4, 5
+print(x, y)
+
+# Easy swap in python
+x, y = y, x 
+print(x, y)
+
+x = 4.234
+print(round(x,-1))
---
+++ FP/YSL_python/demo2.py
@@ -0,0 +1,16 @@
+# String methods
+
+demo = "The quick brown Fox jumps over the lazy Dog"
+print(demo)
+
+
+print(demo.upper())
+
+print(demo.lower())
+
+print(demo.capitalize())
+
+print(demo.title())
+
+exit()
+
