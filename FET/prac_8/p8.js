5fd88b4c8e7f02cada809012f04873cf76e1ea69
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2023-02-10 08:37:05 +0530

    updated

---
+++ FET/prac_8/p8.js
@@ -0,0 +1,14 @@
+function haribol()
+{
+     let h = prompt("Is Kayo's health well?");
+     if("yes" in h || "ok" in h)
+     {    
+          let health = true;
+     }
+     
+     let prmse =  new Promise(function(resolve, reject)
+     {
+
+     });
+     
+}
