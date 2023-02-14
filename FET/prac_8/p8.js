75c0d203dae51d96c4fc367405cd01eaa12d008f
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2023-02-14 12:29:08 +0530

    updated

--- FET/prac_8/p8.js
+++ FET/prac_8/p8.js
@@ -21,22 +21,33 @@ function haribol()
      {
           if(health)
           {
-               resolve(function()
-               {
-                    alert("Great! Your health is well! Nice to hear that!");
-                    cakes = prompt("So how many cakes can you bake?");
-                    while(!Number.isInteger(parseInt(cakes)))
-                    {
-                         cakes = prompt("Please enter valid number of cakes you can bake?");
-                    } 
-                    cakes = parseInt(cakes);
-                    alert(`Thank you for baking ${cakes.toString} cakes for me Kayo!`);
-               });
+               resolve();
           }
           else
           {
-               reject(alert("Sorry to hear that your health is unwell!\nGet well soon Kayo!"));
+               reject();
           }
      });
-     prmse.finally(alert("Thanks for giving me party Kayo!"));
+     prmse.then(
+          (result) =>
+          {
+               alert("Great! Your health is well! Nice to hear that!");
+               cakes = prompt("So how many cakes can you bake?");
+               while(!Number.isInteger(parseInt(cakes)))
+               {
+                    cakes = prompt("Please enter valid number of cakes you can bake?");
+               } 
+               cakes = parseInt(cakes);
+               alert(`Thank you for baking ${cakes} cakes for me Kayo!`);
+          },
+          (error) => {
+               alert("Sorry to hear that your health is unwell!\nGet well soon Kayo!");
+          }
+     ).finally(() => 
+     {
+          alert("Thanks for giving me party Kayo!")
+     });
 }
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', haribol);
