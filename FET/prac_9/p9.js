d192825bd485f90d06a63420c4d23afb1eb4393a
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2023-02-17 09:06:42 +0530

    updated

---
+++ FET/prac_9/p9.js
@@ -0,0 +1,28 @@
+function html()
+{
+     document.getElementById('html').style.display="block";
+}
+function css()
+{
+     document.getElementById('css').style.display="block";
+}
+function js()
+{
+     document.getElementById('js').style.display="block";
+}
+function dom()
+{
+     document.getElementById('dom').style.display="block";
+}
+function dhtml()
+{
+     document.getElementById('dhtml').style.display="block";
+}
+function none()
+{
+     document.getElementById('html').style.display="none";
+     document.getElementById('css').style.display="none";
+     document.getElementById('js').style.display="none";
+     document.getElementById('dom').style.display="none";
+     document.getElementById('dhtml').style.display="none";
+}
