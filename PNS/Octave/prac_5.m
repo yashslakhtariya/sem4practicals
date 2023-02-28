da2f80d523265623d8b9cbd1f9b3c58ead5ec79c
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2023-02-28 19:48:06 +0530

    updated

--- PNS/Octave/prac_5.m
+++ PNS/Octave/prac_5.m
@@ -2,7 +2,7 @@ clear all;
 close all;
 clc;
 
-h = 0.01;
+h = 0.001;
 x = 0:h:1;
 
 ##for i = 1:length(x)
@@ -38,3 +38,31 @@ prbblty = length(find(yrgen < y))/k;
 area = prbblty * (max(x)-min(x)) * (max(y)-min(y));
 
 hold off
+
+count = 0;
+
+for i = 1:length(x)
+  c(i) = length(find(xrgen(find(yrgen < y)) <= x(i)));
+  cdf(i) = c(i) / length(find(yrgen < y));
+  count = count + 1;
+  if(cdf(i) > 0.5)
+    tmp1 = i - 1;
+    break;
+  endif
+endfor
+
+for i = 1:length(x)
+  c(i) = length(find(xrgen(find(yrgen < y)) <= x(i)));
+  cdf(i) = c(i) / length(find(yrgen < y));
+  count = count + 1;
+  if(cdf(i) > 0.95)
+    tmp2 = i - 1;
+    break;
+  endif
+endfor
+
+p1 = x(tmp1);
+p2 = x(tmp2);
+
+printf("\n\tArea : %f\n\tProbability : %f\n\tThe point were CDF(X) is 0.5 : %f\n\tThe point where CDF(x) is 0.95 : %f\n", area, prbblty, p1, p2);
+
