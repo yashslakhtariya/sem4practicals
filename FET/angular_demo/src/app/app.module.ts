12fdd76318df7b14629256840d9bb04bc88c369c
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2023-03-07 09:00:38 +0530

    updated

--- FET/angular_demo/src/app/app.module.ts
+++ FET/angular_demo/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ChildComponent } from './child/child.component';
+import { ParentComponent } from './parent/parent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChildComponent
+    ChildComponent,
+    ParentComponent
   ],
   imports: [
     BrowserModule, FormsModule
--- FET/angular_demo/src/app/child/child.component.ts
+++ FET/angular_demo/src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./child.component.css']
 })
 export class ChildComponent {
+  @Input() amnt: number = 0;
+  @Output() amntChange: EventEmitter<number> = new EventEmitter<number>;
 
+  withdraw()
+  {
+    if (this.amnt <= 0 || (this.amnt - 6400) <= 0)
+    {
+      alert("Insufficient balance!");
+    }
+    else
+    {
+      this.amnt -= 6400;
+      this.amntChange.emit(this.amnt);
+    }
+  }
 }
