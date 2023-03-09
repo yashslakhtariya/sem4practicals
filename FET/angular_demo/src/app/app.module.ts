d13f90faa1d3c2fefc63a746647fe826c6ebd8fe
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2023-03-09 12:21:46 +0530

    updated

--- FET/angular_demo/src/app/app.module.ts
+++ FET/angular_demo/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ChildComponent } from './child/child.component';
 import { ParentComponent } from './parent/parent.component';
 import { Child2Component } from './child2/child2.component';
+import { NgifComponent } from './ngif/ngif.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChildComponent,
     ParentComponent,
-    Child2Component
+    Child2Component,
+    NgifComponent
   ],
   imports: [
     BrowserModule, FormsModule
--- FET/angular_practicals/src/app/app.module.ts
+++ FET/angular_practicals/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Prac11Component } from './prac11/prac11.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Prac11Component
   ],
   imports: [
     BrowserModule
