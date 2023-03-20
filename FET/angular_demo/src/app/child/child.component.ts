0384c8883fcce6df264f60da0dc5a8cc3adecc9a
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2023-03-20 06:52:57 +0530

    updated

--- FET/angular_demo/src/app/child/child.component.ts
+++ FET/angular_demo/src/app/child/child.component.ts
@@ -1,13 +1,26 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, DoCheck, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent {
+export class ChildComponent implements OnChanges, DoCheck, OnInit{
   @Input() amnt: number = 0;
   @Output() amntChange: EventEmitter<number> = new EventEmitter<number>;
+
+  ngOnChanges(changes: SimpleChanges): void
+  {
+    console.log(`OnChanges in Child Component : ${changes}`);
+  }
+
+  ngDoCheck(): void {
+      console.log(`DoCheck in Child Component`);
+  }
+  
+  ngOnInit(): void {
+      console.log(`OnInit in Child Component`);
+  }
 
   withdraw()
   {
