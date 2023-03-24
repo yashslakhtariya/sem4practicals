fd68d10460847445234541ea2e0f1c9a6bbe87aa
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2023-03-24 09:19:47 +0530

    updated

--- FET/angular_practicals/src/app/prac13/prac13.component.ts
+++ FET/angular_practicals/src/app/prac13/prac13.component.ts
@@ -9,10 +9,10 @@ export class Prac13Component
 {
   genre!: number;
 
-  comedy: cmdy[] = [{mv: 'comedy1'}, {mv: 'comedy2'}, {mv: 'comedy3'}, {mv: 'comedy4'}, {mv: 'comedy5'}];
-  action: act[] = [{mv: 'action1'}, {mv: 'action2'}, {mv: 'action3'}, {mv: 'action4'}, {mv: 'action5'}];
-  thriller: thrl[] = [{mv: 'thriller1'}, {mv: 'thriller2'}, {mv: 'thriller3'}, {mv: 'thriller4'}, {mv: 'thriller5'}]
-  horror: hrr[] = [{mv: 'horror1'}, {mv: 'horror2'}, {mv: 'horror3'}, {mv: 'horror4'}, {mv: 'horror5'}]
+  comedy: cmdy[] = [{mv: '3 Idiots (Genre: Comedy, Year: 2009)'}, {mv: 'Free Guy (Genre: Comedy, Year: 2021)'}, {mv: 'Dolittle (Genre: Comedy, Year: 2020)'}, {mv: 'Zero (Genre: Comedy, Year: 2018)'}, {mv: 'Entertainment (Genre: Comedy, Year: 2014)'}];
+  action: act[] = [{mv: 'Free Guy (Genre: Action, Year: 2021)'}, {mv: 'RRR (Genre: Action, Year: 2022)'}, {mv: 'Vikram (Genre: Action, Year: 2022)'}, {mv: 'Don2 (Genre: Action, Year: 2011)'}, {mv: 'Baby (Genre: Action, Year: 2015)'}];
+  thriller: thrl[] = [{mv: 'M3GAN (Genre: Thriller, Year: 2022)'}, {mv: 'Fall (Genre: Thriller, Year: 2022)'}, {mv: 'Predestination (Genre: Thriller, Year: 2014)'}, {mv: 'Lucy (Genre: Thriller, Year: 2014)'}, {mv: 'Bell Bottom (Genre: Thriller, Year: 2021)'}]
+  horror: hrr[] = [{mv: 'Smile (Genre: Horror, Year: 2022)'}, {mv: 'The Witch (Genre: Horror, Year: 2015)'}, {mv: 'M3GAN (Genre: Horror, Year: 2022)'}, {mv: 'Orphan (Genre: Horror, Year: 2009)'}, {mv: 'Roohi (Genre: Horror, Year: 2021)'}]
 }
 
 class cmdy
