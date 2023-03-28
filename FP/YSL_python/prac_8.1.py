4a4e4f74f33e1f43871f0c8feda6a36377e34089
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2023-03-28 12:48:20 +0530

    updated

--- FP/YSL_python/prac_8.1.py
+++ FP/YSL_python/prac_8.1.py
@@ -8,36 +8,36 @@ class BnkAc:
           self.min_blnc = 1000
           
           if self.blnc < self.min_blnc:
-               raise ValueError(f"Minimum Balance of {self.min_blnc} required")
+               raise ValueError(f"Minimum Balance of Rs.{self.min_blnc} required")
     
      @property
      def get_blnc(self):
-          print(f'\nBalance : Rs.{self.blnc}')
+          printMGNTA(f'\nBalance : Rs.{self.blnc}')
     
      def set_blnc(self, setblnc):
           if setblnc < self.min_blnc:
-               raise ValueError(f"Minimum Balance of {self.min_blnc} required")
+               raise ValueError(f"Minimum Balance of Rs.{self.min_blnc} required")
           else:
                self.blnc = setblnc
                printBLU(f"\nBalance set : Rs.{self.blnc}")
           
      def deposit(self, amount):
           self.blnc += amount
-          printGRN(f'\nDeposited : Rs.{amount}\nCurrent Balance : {self.blnc}')
+          printGRN(f'\nDeposited : Rs.{amount}\nCurrent Balance : Rs.{self.blnc}')
           
      def withdraw(self, amount):
           if self.blnc - amount < self.min_blnc:
-               raise ValueError(f"Minimum Balance of {self.min_blnc} required")
+               raise ValueError(f"Minimum Balance of Rs.{self.min_blnc} required")
           else:
                self.blnc -= amount
-               printORNG(f"\nWithdrawn : Rs.{amount}\nCurrent Balance : {self.blnc}")
+               printORNG(f"\nWithdrawn : Rs.{amount}\nCurrent Balance : Rs.{self.blnc}")
           
      def __str__(self):
-          return f"\nName: {self.name}\nAccount Type: {self.ac}\nBalance: {self.blnc}"
+          return f"\nAccount Holder : {self.name}\nAccount Type : {self.ac}\nBalance : Rs.{self.blnc}"
    
 ysl = BnkAc('YSL', 'Savings', 64321616)
 ysl.deposit(64000)
 ysl.withdraw(64000)
 ysl.get_blnc
 ysl.set_blnc(6464323216)
-printMGNTA(str(ysl))
+print(str(ysl))
