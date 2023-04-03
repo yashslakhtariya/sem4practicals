45ef3be15b52d0a70d7d31403d8a09af178d1717
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2023-04-03 12:42:10 +0530

    updated

--- FP/YSL_python/demo997.py
+++ FP/YSL_python/demo997.py
@@ -1,10 +1,11 @@
 import matplotlib
+import numpy as np
 import matplotlib.pyplot as plt
 matplotlib.use('GTK3Agg')
 
-values = [5,8,9,4,1,6,7,2,3,8]
+values = np.array([5,8,9,4,1,6,7,2,3,8])
 f1 = plt.figure(1)
-plt.plot(range(1,11), values, c='r', ls='--', marker='*')
+plt.plot(range(1,11), values, c='#b75969', ls='--', marker='*')
 # plt.show()
 
 values2= [305, 436, 805, 35, 512, 640]
@@ -16,11 +17,11 @@ plt.pie(values2, colors=c, labels=l, explode=e, autopct='%1.3f%%')
 plt.legend()
 # plt.show()
 
-x = [1,2,3,4,5]
-y = [8.7, 9.3, 9.2, 9.9, 9.5]
-c = ['#5e81cc', '#2aa1b3', '#BF616C', '#eaca8a', '#a347ba']
-l = ['Sem1', 'Sem2', 'Sem3', 'Sem4', 'Sem5']
-w = [0.5, 0.6, 1.2, 0.5, 0.9]
+x = np.array([1,2,3,4,5])
+y = np.array([8.7, 9.3, 9.2, 9.9, 9.5])
+c = np.array(['#5e81cc', '#2aa1b3', '#BF616C', '#eaca8a', '#a347ba'])
+l = np.array(['Sem1', 'Sem2', 'Sem3', 'Sem4', 'Sem5'])
+w = np.array([0.5, 0.6, 1.2, 0.5, 0.9])
 f3 = plt.figure(3)
 plt.bar(x, y, label=l, width=w, color=c)
 plt.title('Sem-wise Score')
